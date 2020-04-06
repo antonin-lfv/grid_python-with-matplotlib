570adab31236df21ff5e5983ef27613096fd4629
Antonin <antoninlefevre45@icloud.com>
2020-04-06 13:17:35 +0200

    Create squaring_python.py

---
+++ squaring_python.py
@@ -0,0 +1,18 @@
+"""creation of a squaring """
+
+def carte_topo2(xmin,xmax,ymin,ymax):
+    #window
+    plt.clf()
+    plt.xlim(xmin,xmax)
+    plt.ylim(ymin,ymax)
+    #params
+    m = min(xmin,ymin)
+    M = max(xmax,ymax)
+    #with plt.style.context('dark_background'): dark mode
+    y = np.array([M,m])
+    x = np.array([m,M])
+    for i in range (m,M+1):
+        for k in range (m,M+1):
+            plt.plot(np.array([i,i]),y,'k-')
+            plt.plot(x,np.array([k,k]),'k-')
+    plt.show()
